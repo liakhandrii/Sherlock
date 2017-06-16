db3994f4a4f66626a3b93a48248788e79d698a05
Andrii Liakh <liakhandrii@gmail.com>
2017-06-16 11:44:45 +0300

    Initial Commit

---
+++ Sherlock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sherlock
+//
+//  Created by Andrii Liakh on 6/16/17.
+//  Copyright © 2017 Twopeople Software LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sherlock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sherlock
+//
+//  Created by Andrii Liakh on 6/16/17.
+//  Copyright © 2017 Twopeople Software LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
